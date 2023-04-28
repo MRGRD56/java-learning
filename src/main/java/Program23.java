616c53be8ba796c217ab56e0d6acd0b1091867fc
k.golikov <kgolikov@37bytes.com>
2023-04-28 20:56:59 +0500

    Some changes - CompletableFuture 2

--- src/main/java/Program23.java
+++ src/main/java/Program23.java
@@ -1,16 +1,20 @@
 import com.mrgrd56.models.people.Gender;
 import com.mrgrd56.models.people.Person;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 public class Program23 {
     private static final Random random = new Random();
 
     public static void main(String[] args) throws InterruptedException {
+        logThread("very beginning");
+
         fetchPerson()
-                .thenApply((person) -> {
-                    System.out.println("Got the person!");
+                .thenApplyAsync((person) -> {
+                    logThread("got the person");
 
                     try {
                         Thread.sleep(500);
@@ -20,23 +24,35 @@ public class Program23 {
 
                     person.setAge(person.getAge() + 1);
 
+                    logThread("Finished processing the person");
+
                     return person;
                 })
-                .thenAccept((person) -> {
+                .thenAcceptAsync((person) -> {
+                    logThread("Printing the person");
+
                     System.out.println(person);
                 });
 
-        System.out.println("After running the future, I'm going to sleep...");
+        logThread("After running the future, I'm going to sleep...");
 
         Thread.sleep(5000);
 
-        System.out.println("Ahhh, good morning...?");
+        logThread("Ahhh, good morning...? Uh, no......");
+
+        Thread.sleep(5000);
+
+        logThread("---Is it already 11 PM?");
     }
 
     private static CompletableFuture<Person> fetchPerson() {
         CompletableFuture<Person> future = new CompletableFuture<>();
 
+        logThread("fetchPerson beginning");
+
         new Thread(() -> {
+            logThread("fetchPerson thread beginning");
+
             try {
                 Thread.sleep(random.nextInt(1000, 2000));
             } catch (InterruptedException e) {
@@ -46,9 +62,15 @@ public class Program23 {
             }
 
             var person = new Person("Some Guy", random.nextInt(12, 40), Gender.MALE);
+            logThread("before future.complete");
             future.complete(person);
-        }).start();
+            logThread("after future.complete");
+        }, "person-fetching").start();
 
         return future;
+    }
+
+    private static void logThread(Object message) {
+        System.out.println("[" + Thread.currentThread().getName() + "] " + message);
     }
 }
